6d1f1fb3f1f6b7ff9643eba8a6f9ec670fb41419
devlwkyan <devlwkyan@protonmail.com>
2018-12-02 20:02:45 -0300

    remake oop

--- jogo.py
+++ jogo.py
@@ -1,24 +1,30 @@
-
-import random
-
-x = input("Jogo da megasena ou lotofácil? ").lower()
-
-if x == "megasena" or X == "mega sena":
-    jogo = []
+class Jogo():
     
-    n = int(input("Quantos jogos você quer fazer? "))
 
-    for i in range(n):
-        jogo.append(random.sample(range(1, 61), 6))
-        jogo.append(" ")
-    print(jogo)
+    def megasena():
+        import random
+        jogo = []
 
-    
-if x == "lotofacil" or x == "lotofácil":
-    jogo = []
-    n = int(input("Quantos jogos você quer fazer? "))
+        n = int(input("Quantos jogos você quer fazer? "))
 
-    for i in range(n):
-        jogo.append(random.sample(range(1, 26), 15))
-        jogo.append(" ")
-print(jogo)
+        for i in range(n):
+            jogo.append(random.sample(range(1, 61), 6))
+            jogo.append(" ")
+        print(jogo)
+
+    def lotofacil():
+        import random
+        jogo = []
+
+        n = int(input("Quantos jogos você quer fazer? "))
+
+        for i in range(n):
+            jogo.append(random.sample(range(1, 61), 6))
+            jogo.append(" ")
+        print(jogo)
+
+    x = input("Jogo da megasena ou lotofácil? ").lower()
+    if x == "megasena" or x == "mega sena":
+        megasena()
+    if x == "lotofacil" or x == "lotofácil":
+        lotofacil()
